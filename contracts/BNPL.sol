9f41e9bd29a3540ae2418dfa35fc2eb1814c7ccf
Saviour1001 <harshghodkar@gmail.com>
2024-09-22 07:02:57 +0800

    updated contract

--- contracts/BNPL.sol
+++ contracts/BNPL.sol
@@ -10,6 +10,52 @@ contract BNPL is OApp {
     // Some arbitrary data you want to deliver to the destination chain!
     string public data;
 
+    // Loan Directory CRUD
+    struct Loan {
+        uint256 id;
+        address borrower;
+        uint256 amount;
+        uint256 dueDate;
+        bool isPaid;
+    }
+
+    mapping(uint256 => Loan) public loans;
+    uint256 public nextLoanId;
+
+    event LoanCreated(uint256 id, address borrower, uint256 amount, uint256 dueDate);
+    event LoanUpdated(uint256 id, uint256 amount, uint256 dueDate, bool isPaid);
+    event LoanDeleted(uint256 id);
+
+    // CRUD Functions for Loan Directory
+
+    function createLoan(address _borrower, uint256 _amount, uint256 _dueDate) external onlyOwner {
+        uint256 loanId = nextLoanId++;
+        loans[loanId] = Loan(loanId, _borrower, _amount, _dueDate, false);
+        emit LoanCreated(loanId, _borrower, _amount, _dueDate);
+    }
+
+    function readLoan(uint256 _loanId) external view returns (Loan memory) {
+        require(loans[_loanId].borrower != address(0), "Loan does not exist");
+        return loans[_loanId];
+    }
+
+    function updateLoan(uint256 _loanId, uint256 _amount, uint256 _dueDate, bool _isPaid) external onlyOwner {
+        require(loans[_loanId].borrower != address(0), "Loan does not exist");
+        Loan storage loan = loans[_loanId];
+        loan.amount = _amount;
+        loan.dueDate = _dueDate;
+        loan.isPaid = _isPaid;
+        emit LoanUpdated(_loanId, _amount, _dueDate, _isPaid);
+    }
+
+    function deleteLoan(uint256 _loanId) external onlyOwner {
+        require(loans[_loanId].borrower != address(0), "Loan does not exist");
+        delete loans[_loanId];
+        emit LoanDeleted(_loanId);
+    }
+
+    // Existing LayerZero functions
+
     /**
      * @notice Sends a message from the source to destination chain.
      * @param _dstEid Destination chain's endpoint ID.
