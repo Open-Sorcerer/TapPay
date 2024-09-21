ac0f8a8242716959766dde14a4d210c736cfe626
Neel <neelp0980@gmail.com>
2024-09-22 02:22:32 +0800

    style: settings page

--- app/wallet.tsx
+++ app/wallet.tsx
@@ -7,6 +7,8 @@ import { Button, Input, Spinner, Text, View, XStack, YStack } from "tamagui";
 import { Modal, StyleSheet, TouchableOpacity } from "react-native";
 import TokenList from "@/components/TokenList";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "expo-router";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 const CustomBottomSheet = ({
   visible,
@@ -106,6 +108,8 @@ function WalletScreen() {
   const encrypted =
     "a5ca001ff7b689d376e726768ed64127:487f70f2a6e1aa10f2e230af92dcb6f8abde428a66a1cd892ce69d56fa3e34e9e6aea5a8b319aa8263b4de29632b6b4b63d2e0b12aebc9c44c80dceaf50686ca9dbeeb881d0d92e2029aae20e76bfc95";
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
   return (
     <>
       {isLoading ? (
@@ -114,10 +118,24 @@ function WalletScreen() {
         </View>
       ) : (
         <YStack flex={1} padding="$4" className="bg-white">
-          <XStack justifyContent="space-between" width="100%" marginBottom="$4">
+          <XStack
+            justifyContent="space-between"
+            width="100%"
+            marginBottom="$4"
+            alignItems="center"
+          >
             <Text fontSize="$7" fontWeight="bold">
               My Wallet
             </Text>
+            <Button
+              backgroundColor="transparent"
+              width="fit"
+              onPress={() => {
+                router.push("/settings");
+              }}
+            >
+              <TabBarIcon name="settings-outline" size={24} />
+            </Button>
           </XStack>
           <Text fontSize="$12" fontWeight="bold">
             <Text color="#a3a3a3" fontWeight="semibold">
